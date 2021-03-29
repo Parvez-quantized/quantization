f22a170ac657e3e260d2fe8917358a2078a07bd9
Parvez-quantized <pervaizdae@yahoo.com>
2021-03-29 13:05:41 +0530

    Descriptive stats code

---
+++ descriptive stats.R
@@ -0,0 +1,56 @@
+
+data <- read.csv(file="D:/INAA.csv", header=TRUE, sep=",")
+data
+class(data)
+View(data)
+write.csv(data, file="data2.csv", quote=TRUE, na="na", row.names=FALSE)
+install.packages("xlsx")
+require("xlsx")
+mydata <- read.xlsx(file="D:/FS 2020-21/Sample Slip/20.02.21.xlsx", 1)
+View(mydata)
+help(read.xlsx)
+class(mydata)
+length(mydata)
+length(data)
+write.xlsx(data, file="data2.xlsx", sheetName="sheet1", col.names=TRUE, row.names=FALSE)
+install.packages("foreign")
+require(foreign)
+install.packages("rjson")
+require(rjson)
+data$x3
+View(data)
+data[, c("Ce", "La")]
+data$La
+data[order(data$La), ]
+data[order(data$La, decreasing=TRUE), ]
+data[order(data$La, data$Lu), ]
+data[data$La > 100, ]
+data[data$La > 100 & data$x < 500, ]
+na.omit(data)
+data[!duplicated(data$La), ]
+y <- table(data$La)
+print(y)
+names(y) [which(y==max(y))]
+median(data$Ce)
+mode(data$La)
+res <- range(data$La)
+diff(res)
+quantile(data$La)
+IQR(data$La, na.rm=TRUE)
+N <- nrow(data)
+var(data$La) * (N - 1) / N
+var(data$Ce)
+N <- nrow(data);
+variance <- var(data$Eu) * (N - 1) / N;
+sqrt(variance)
+sd(data$Eu)
+hist(data$La, breaks=10)
+qqnorm(data$La)
+qqline(data$La)
+shapiro.test(data$La)
+installed.packages("moments")
+require(moments)
+skewness(data$La)
+kurtosis(data$La)
+summary(data)
+str(data)
